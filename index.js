6edc71e191a0943519fc613906acaf1dfcab60c8
Chris Choi <chrischoi96@gmail.com>
2020-11-08 19:24:15 -0500

    semicolon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs/index');
 const resolvers = require('./resolvers/index');
-const dataSources = require('./datasources/index')
+const dataSources = require('./datasources/index');
 const apiKey = process.env.RIOT_API_KEY;
 
 const server = new ApolloServer({
